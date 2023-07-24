ccad14dd25bf4487a7b5a31b2231a3ab6dde112f
jaximuss <chidiumeo@gmail.com>
2023-07-24 20:47:41 +0100

    just commiting for commit sake, app will change its storage from sqlite to a textfile

---
+++ DbContext/TaskDbContext.cs
@@ -0,0 +1,22 @@
+﻿using AppTest.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTest.DbContext
+{
+    public class TaskDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DbSet<TasksModel> Tasks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "tasks.db");
+            optionsBuilder.UseSqlite($"Data Source={dbPath}");
+        }
+    }
+
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,27 +1,68 @@
-﻿using Microsoft.Office.Interop.Word;
+﻿using AppTest.DbContext;
+using AppTest.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Office.Interop.Word;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace AppTest;
 
 public partial class MainPage : ContentPage
 {
-	private List<string> tasks = new List<string>();
+    private List<string> tasks = new List<string>();
 
-	public MainPage()
-	{
-		InitializeComponent();
-		taskListView.ItemsSource = tasks;	
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+        RefreshTasksList();
+    }
 
     private void OnAddTaskClicked(object sender, EventArgs e)
     {
-        string taskText = taskEntry.Text;
-        if (!string.IsNullOrWhiteSpace(taskText))
+        try
         {
-            tasks.Add(taskText);
-            taskEntry.Text = string.Empty; // Clear the entry field after adding task
-            taskListView.ItemsSource = null;
-            taskListView.ItemsSource = tasks; // Update the ListView's item source to reflect the changes
+            string taskText = taskEntry.Text;
+            if (!string.IsNullOrWhiteSpace(taskText))
+            {
+                using (var db = new TaskDbContext())
+                {
+                    TasksModel task = new TasksModel { TaskText = taskText };
+                    db.Tasks.Add(task);
+                    db.SaveChanges();
+                }
 
+                taskEntry.Text = string.Empty; // Clear the entry field after adding task
+                RefreshTasksList();
+            }
+        }
+        catch (DbUpdateException ex)
+        {
+            // Handle database-related exceptions and log the inner exception details
+            Console.WriteLine("Database Error: " + ex.Message);
+            Console.WriteLine("Inner Exception: " + ex.InnerException?.Message);
+            Console.WriteLine("StackTrace: " + ex.StackTrace);
+        }
+        catch (Exception ex)
+        {
+            // Catch any other unexpected exceptions
+            Console.WriteLine("Unexpected Error: " + ex.Message);
+            Console.WriteLine("StackTrace: " + ex.StackTrace);
+        }
+    }
+
+    private void RefreshTasksList()
+    {
+        try
+        {
+            using (var db = new TaskDbContext())
+            {
+                var tasks = db.Tasks.Select(t => t.TaskText).ToList();
+                taskListView.ItemsSource = tasks;
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine("Error: " + ex.Message);
         }
     }
 }
